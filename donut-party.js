5b4068b0f449cf71a9ed19e2691da7bf1cb59369
ahimmelstoss <ahimmelstoss@gmail.com>
2013-11-14 15:01:36 -0500

    form added to calculate donuts; responsive text and images

--- donut-party.js
+++ donut-party.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
 
-var numGuests = parseInt(prompt("How many guests?"));
+  $('#count').submit(function(){
 
-var numDonuts = parseInt(prompt("How many donuts?"));
+    var numGuests = parseInt($(this).find('input:eq(0)').val());
 
-if(numGuests < numDonuts){
-  $('p').text("We are happy! So many donuts!");
-}else if(numGuests > numDonuts){
-  $('p').text("We don't have enough donuts!!! :(");
-}else{
-  $('p').text("I guess we have enough donuts...");
-}
+    var numDonuts = parseInt($(this).find('input:eq(1)').val());
 
+    if(numGuests < numDonuts){
+      $('h2').html("Yes! So many donuts!");
+      $('#return').append('<img src="http://www.us.all.biz/img/us/catalog/8653.jpeg" alt="many">');
+    }else if(numGuests > numDonuts){
+      $('h2').html("We don't have enough donuts!!! :(");
+      $('#return').append('<img src="http://cdn02.cdnwp.celebuzz.com/wp-content/uploads/2013/05/18/tina-fey-macncheese.gif" alt="many">');
+    }else{
+      $('h2').html("I guess we have enough donuts...");
+      $('#return').append('<img src="http://www.windmillcandies.com/product_images/o/546/Potato-Donuts__65295_zoom.jpg" alt="many">');
+    }
+
+    return false;
+
+  });
 });
